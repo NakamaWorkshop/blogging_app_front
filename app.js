aaa24c69e4f0983e0c23e4e1a5b9144a218ade83
Nicolas Oga <2112.oga@gmail.com>
2017-09-09 15:11:51 -0300

    🚧  WIP: auth system

--- app.js
+++ app.js
@@ -5,30 +5,81 @@ var slideout = new Slideout({
   tolerance: 70
 });
 
+$('.js-login-button').click(ev => {
+  $('backdrop').fadeIn(1000);
+});
+
+$('body').click(ev => {
+  if (ev.target == $('backdrop')[0]) $('backdrop').fadeOut();
+});
+
 $('.js-login-form').submit(ev => {
   ev.preventDefault();
-  // Tarea para el hogar: obtener los valores que el usuario ingreso
-  // en los campos para email y contraseña y mostralos usando alert()
+});
+
+var users = [];
+
+// Ejemplo de uso de .find() o filter():
+[1,2,3,4].find(e => e % 2 === 0);;
+[{color: 'red'}].find(e => e.color === 'red');
+if ([{color: 'rd'}].find(e => e.color === 'red')) alert('lo encontre!');
+if ([{color: 'red'}].find(e => e.color === 'red')) alert('lo encontre!');
+
+// Tarea para el hogar:
+//  - nodejar registrarse si ya existe un usuario con el mismo email
+//  - no dejar registrarse sino concide la contraseña y la confirmación de contraseña
+$('.js-register-form').submit(ev => {
+  ev.preventDefault();
+
+  var $form       = $(ev.target);
+  var inputValues = collectInputValues($form);
+
+
+  if (condition) {
+    users.push({
+      email:    inputValues.email,
+      password: inputValues.password
+    });
+
+    alert('Gracias por registrarte!');
+
+    $('backdrop').fadeOut();
+  }
 });
 
 $('.js-post-form').submit(ev => {
   ev.preventDefault();
 
-  var $titleInput = $('input[name="title"]');
-  var $bodyInput  = $('textarea[name="body"]');
-
-  var title = $titleInput.val();
-  var body  = $bodyInput.val();
+  var $form       = $(ev.target);
+  var inputValues = collectInputValues($form);
 
   var $post = $(`
     <div class='post'>
-      <h3>${title}</h3>
-      <p>${body}</p>
+      <h3>${inputValues.title}</h3>
+      <p>${inputValues.body}</p>
     </div>
   `);
 
   $('.posts-container').append($post);
 
-  $titleInput.val('');
-  $bodyInput.val('');
+  resetInputValues($form);
 });
+
+function collectInputValues($form) {
+  var res = {};
+
+  $form.find('input, textarea').each((_i, e) => {
+    var inputName  = $(e).attr('name');
+    var inputValue = $(e).val();
+
+    res[inputName] = inputValue;
+  });
+
+  return res;
+}
+
+function resetInputValues($form) {
+  $form
+    .find('input, textarea')
+    .each((_i, e) => $(e).val(''));
+}
